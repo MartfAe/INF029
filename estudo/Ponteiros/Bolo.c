b635657dff8233b15e811b381f2385f4b5c42d17
MartfAe <anaemilia@aluno.ufrb.edu.br>
2025-02-13 12:53:00 -0300

    ultimo teste

---
+++ estudo/Ponteiros/Bolo.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void comerBolo(int fatias){
+    char resposta;
+    if(fatias ==){
+        printf("O bolo acabou!\n");
+        return;
+    }
+    
+
+}
+
+int main(){}
