81915525c51c68f89ca258893a7b9a402e3bb9d4
MartfAe <anaemilia@aluno.ufrb.edu.br>
2025-02-13 10:00:00 -0300

    ultimo teste

--- estudo/Ponteiros/Bolo.c
+++ estudo/Ponteiros/Bolo.c
@@ -3,12 +3,22 @@
 
 void comerBolo(int fatias){
     char resposta;
-    if(fatias ==){
+    if(fatias ==0){
         printf("O bolo acabou!\n");
         return;
     }
     
-
+int idade;
+    printf("Quantas fatias de bolo você quer comer?\n");
+    scanf("%d", &fatias);
+    comerBolo(fatias);
+    printf("Deseja comer mais bolo? (s/n)\n");
+    scanf(" %c", &resposta);
+    if(resposta == 's'){
+        comerBolo(fatias);
+    }
+    else{
+        printf("Você comeu %d fatias de bolo.\n", fatias);
+    }
 }
-
 int main(){}
