54197e6d4dd63e5f65bdf5498437a5557a03ab58
Ana Emília Martfeld <anaemilia@aluno.ufrb.edu.br>
2025-04-01 21:18:08 +0000

    projetoEscola semestre 25.1

--- 25.1/ProjetoEscola/projetoEscola.c
+++ 25.1/ProjetoEscola/projetoEscola.c
@@ -1,25 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h> 
-#include <math.h>
 
-int soma(float n1, float n2){ //recebe dois valores, n1 e n2 e retorna a soma de ambos
-    float resultado;
-    resultado = n1 + n2 ;
-    return(resultado);
-}
-int main(){
-    float v1 , v2, resultado; 
-    printf("Digite o primeiro valor:\n");
-    scanf("%f", &v1);
-    printf("Digite o segundo valor:\n");
-    scanf("%f", &v2);
-    
-    //Chama a função e recebe o retorno
-    resultado = soma(v1,v2);
+#define maxAlunos 100
+#define maxProfessores 100
+#define maxDisciplinas 100
+#define maxAlunosPorDisciplina 100
+#define maxMatricula
 
-    printf("Resultado = %.1f\n", resultado);
+#define prefixoAluno '2025106'
+#define prefixoProfessor '2025117'
 
-   // getch();
-    return 0 ; 
 
-}
+// Definindo as Structs
+typedef struct{
+    int dia, mes, ano;
+}data;
+
+
+typedef struct{
+    char nome[50];
+    char sexo;
+    char matricula[maxMatricula];
+    data aniversario;
+    int disciplinas;
+}pessoas;
+
+typedef struct{
+    char nome[50];
+    char codigo[9];
+    char professor[50];
+    float semestre;
+    pessoas aluno[maxAlunosPorDisciplina];
+    int numAlunos;
+    int numVagas;
+    int temProfessor;
+}materias;
+
+
+//Definindo as funções. 
+
+void cadastrarPessoa(pessoas *cadastro, int maxPessoas, char tipoPessoa);
+void cadastrarDisciplina(materias *disciplinas, int maxDisciplinas, pessoas *professores, int maxDisciplinas);
+void gerarMatriculaAluno(char matricula[]);
+void gerarMatriculaProfessor(char matricula[]);
+
+//Funções de validação
+void validarNome(char nome[]);
+void validarSexo(char sexo);
+void validarCpf(char cpf[]);
+int validarData(data aniversario);
+int validarCodigoDisciplina(char codigo[]);
+int validarSemestre(float semestre);
+void toUpperString(char *str);
+
+int contadorAluno = 0;
+int contadorProfessor = 0;
+
